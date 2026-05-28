8ea1ddd1db7709c63ab0593f5a5921086b3b6a3e
andrewcrash <andrey.search26@gmail.com>
2026-05-28 17:46:05 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('.faq-question').forEach(q=>{
+ q.addEventListener('click',()=>{
+   const a=q.nextElementSibling;
+   a.style.display=a.style.display==='block'?'none':'block';
+ });
+});
